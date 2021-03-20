2ac86892187220abe39b35e4a6b4b691bb9427ab
Oleg Karmanskyy <oleg.karman@gmail.com>
2021-03-20 22:49:43 +0200

    forms refacroting

--- src/containers/Todos/TodoDetailPage/TodoDetailPage.js
+++ src/containers/Todos/TodoDetailPage/TodoDetailPage.js
@@ -1,75 +1,11 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
 import { connect } from 'react-redux';
 import { getTodosById } from '../../../redux/selectors/todosSelectors';
-import { todoStatus } from '../../../utils/constants';
-import { updateTodos } from '../../../redux/actions/todosActions';
+import TodoForm from '../TodoForm/TodoForm';
 
-const TodoDetailsSchema = Yup.object().shape({
-    title: Yup.string()
-        .min(2, 'Too Short!')
-        .max(50, 'Too Long!')
-        .required('Required'),
-    description: Yup.string()
-        .min(2, 'Too Short!')
-        .max(250, 'Too Long!')
-        .required('Required'),
-    status: Yup.string()
-});
-
-const TodoDetailPage = ({todoDetails, updateTodo}) => {
-    const renderStatusOptions = () => {
-        return Object.values(todoStatus).map((status, i) => {
-            return <option key={i} value={status}>{status}</option>
-        });
-    };
-
-    const drawUpdateButtons = () => (
-        <>
-            <button type="submit">Update</button>
-            <a href={'/todos'}>Cancel</a>
-        </>
-    );
-
-    const prepareTodoToUpdate = values => {
-        const updatedValues = {...values, id: todoDetails.id};
-        updateTodo(updatedValues);
-    };
-
+const TodoDetailPage = ({todoDetails}) => {
     return (
-        <div>
-            {/* {JSON.stringify(todoDetails)} */}
-
-            <Formik
-                initialValues={{
-                    title: todoDetails.title,
-                    description: todoDetails.description,
-                    status: todoDetails.status,
-                    date: todoDetails.date
-                }}
-                validationSchema={TodoDetailsSchema}
-                onSubmit={prepareTodoToUpdate}
-            >
-            {({ errors, touched }) => (
-                <Form>
-                    <Field name='title' />
-                    {errors.title && touched.title ? (
-                        <div>{errors.title}</div>
-                    ) : null}
-                    <Field name='description' />
-                    {errors.description && touched.description ? (
-                        <div>{errors.description}</div>
-                    ) : null}
-                    <Field as='select' name='status'>
-                        {renderStatusOptions()}
-                    </Field>
-                    <p>{todoDetails.date}</p>
-                    {Object.keys(touched).length ? drawUpdateButtons() : null}
-                </Form>
-            )}
-            </Formik>
-        </div>
+        <TodoForm updateTodoForm={true} todoDetails={todoDetails} />
     );
 };
 
@@ -78,8 +14,4 @@ const mapStateToProps = (state, dispatchProps) => {
     return { todoDetails }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-    updateTodo: (todo) => dispatch(updateTodos(todo))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(TodoDetailPage);
+export default connect(mapStateToProps)(TodoDetailPage);
--- src/containers/Todos/TodoForm/TodoForm.js
+++ src/containers/Todos/TodoForm/TodoForm.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux'
  import { Formik, Form, Field } from 'formik';
  import * as Yup from 'yup';
-import { addTodos } from '../../../redux/actions/todosActions';
- 
-const TodoSchema = Yup.object().shape({
+import { addTodos, updateTodos } from '../../../redux/actions/todosActions';
+import { todoStatus } from '../../../utils/constants';
+
+const formBasicInputs = {
     title: Yup.string()
         .min(2, 'Too Short!')
         .max(50, 'Too Long!')
@@ -13,31 +14,84 @@ const TodoSchema = Yup.object().shape({
         .min(2, 'Too Short!')
         .max(250, 'Too Long!')
         .required('Required')
+};
+
+const formBasicInitialValues = {
+    title: '',
+    description: ''
+};
+ 
+const TodoSchema = Yup.object().shape({
+    ...formBasicInputs
+});
+
+const TodoDetailsSchema = Yup.object().shape({
+    ...formBasicInputs,
+    status: Yup.string()
 });
  
-const TodoForm = ({ createNewTodo }) => {
+const TodoForm = ({ updateTodoForm = false, todoDetails = {}, createNewTodo, updateTodos }) => {
+    const textValue = updateTodoForm ? 'Update' : 'Create';
+    const validationSchema = updateTodoForm ? TodoDetailsSchema : TodoSchema;
+    const initialValues = updateTodoForm ? {
+        title: todoDetails.title,
+        description: todoDetails.description,
+        status: todoDetails.status,
+        date: todoDetails.date
+    } : formBasicInitialValues;
+
+    const prepareTodoToUpdate = values => {
+        const updatedValues = {...values, id: todoDetails.id};
+        console.log(updatedValues);
+        updateTodos(updatedValues);
+    };
+    
+    const onSubmit = updateTodoForm ? prepareTodoToUpdate : createNewTodo;
+
+    const drawDetailsFields = () => {
+        return (
+            <>
+                <Field as='select' name='status' className="status-input" >
+                    {renderStatusOptions()}
+                </Field>
+                <p className='date-text'>{todoDetails.date}</p>
+            </>
+        );
+    };
+
+    const renderStatusOptions = () => {
+        return Object.values(todoStatus).map((status, i) => {
+            return <option key={i} value={status}>{status}</option>
+        });
+    };
+
+    const drawUpdateButtons = () => (
+        <div className='form-contols'>
+            <button type="submit">{textValue}</button>
+            {updateTodoForm ? <a href={'/todos'}>Cancel</a> : null}
+        </div>
+    );
+
     return (
-        <div>
-            <h1>Create Todo</h1>
+        <div className='form-wrapper'>
+            <h1 className='form-header'>{textValue} TODO</h1>
             <Formik
-                initialValues={{
-                    title: '',
-                    description: ''
-                }}
-                validationSchema={TodoSchema}
-                onSubmit={createNewTodo}
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={onSubmit}
             >
             {({ errors, touched }) => (
                 <Form>
-                    <Field name='title' />
+                    <Field name='title' className="title-input" />
                     {errors.title && touched.title ? (
                         <div>{errors.title}</div>
                     ) : null}
-                    <Field name='description' />
+                    <Field name='description' className='description-input' />
                     {errors.description && touched.description ? (
                         <div>{errors.description}</div>
                     ) : null}
-                    <button type='submit'>Create</button>
+                    {updateTodoForm ? drawDetailsFields() : null}
+                    {drawUpdateButtons()}
                 </Form>
             )}
             </Formik>
@@ -46,7 +100,8 @@ const TodoForm = ({ createNewTodo }) => {
 };
 
 const mapDispatchToProps = (dispatch) => ({
-    createNewTodo: (todo) => dispatch(addTodos(todo))
+    createNewTodo: (todo) => dispatch(addTodos(todo)),
+    updateTodos: (todo) => dispatch(updateTodos(todo))
 });
   
 export default connect(null, mapDispatchToProps)(TodoForm);
