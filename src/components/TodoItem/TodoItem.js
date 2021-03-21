560380f2fe4f34f7125e8a176a4870dfee25d452
Oleg Karmanskyy <oleg.karman@gmail.com>
2021-03-21 10:34:06 +0200

    todos status styles

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -3,7 +3,7 @@ import './TodoItem.css';
 
 const TodoItem = ({onDelete, todo}) => {
     return (
-        <div className='todo-item-wrapper'>
+        <div className={`todo-item-wrapper status-${todo.status}`}>
             <button onClick={() => onDelete(todo.id)}>X</button>
             <a href={`/todos/${todo.id}`}>
                 <h3 className='todo-item-header'>{todo.title}</h3>
